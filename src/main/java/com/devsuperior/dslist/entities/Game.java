d38f4f46b5190ee64b06f3e854ccc2c171349fd7
joao pedro barros <jppn123>
2023-07-11 13:46:47 -0300

    adicionando povoamento da tabela tb_game

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -13,21 +13,33 @@ public class Game {
     private String title;
     @Column(name = "game_year")//customizar nome no banco
     private Integer year;
+    private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game(Long id, String title, Integer year, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id,String genre, String title, Integer year, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
+        this.genre = genre;
         this.year = year;
         this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public Long getId() {
