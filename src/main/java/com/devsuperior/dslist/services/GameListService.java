99ed4fa556ed4dedb6702995b214d9f6ffd3957c
joao pedro barros <jppn123>
2023-07-12 12:04:47 -0300

    adicionando GameList service, controller, DTO, repository

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,19 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-public class GameService {
+public class GameListService {
 
     @Autowired
-    private GameRepository gameRepository;
+    private GameListRepository gameListRepository;
     @Transactional(readOnly = true)
-    public List<GameMinDTO> findAll(){
-        List<Game> result = gameRepository.findAll();
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
         //transforma uma base de dados em outra base de dados utilizando o map da função stream, caso seja com uma lista
-        return result.stream().map(x -> new GameMinDTO(x)).toList();
-    }
-    @Transactional(readOnly = true)
-    public GameDTO findById(Long id){
-        Game result = gameRepository.findById(id).get();
-        return new GameDTO(result);
+        return result.stream().map(x -> new GameListDTO(x)).toList();
     }
 }
